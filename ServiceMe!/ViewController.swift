f9b3d6ce584ec6dfbe4aa2a0375b3d95bc057088
George Kunthara <gkunthara@georges-macbook-pro.local>
2017-04-06 16:03:43 -0700

    Initial Commit

---
+++ ServiceMe!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ServiceMe!
+//
+//  Created by George Kunthara on 4/6/17.
+//  Copyright © 2017 George&Evan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
