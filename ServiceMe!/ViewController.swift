f82d30b0898faa445d95deb811b5d65ec0d78017
George Kunthara <gkunthara@georges-macbook-pro.local>
2017-04-06 21:40:39 -0700

    Question Page
    Added a new page with yes and no buttons

--- ServiceMe!/ViewController.swift
+++ ServiceMe!/ViewController.swift
@@ -2,13 +2,19 @@
 //  ViewController.swift
 //  ServiceMe!
 //
-//  Created by George Kunthara on 4/6/17.
+//  Created by George Kunthara & Evan Arends on 4/6/17.
 //  Copyright © 2017 George&Evan. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func volunteerButton(_ sender: Any) {
+        
+        //does the segue from the homepage to the start of the Questionaire
+        performSegue(withIdentifier: "homeToQuestionSegue", sender: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
