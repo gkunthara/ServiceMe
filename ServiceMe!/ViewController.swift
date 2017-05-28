a100eadf29e5933088762a167d890b9b7c0bf79b
gkunthara <gkunthara1@gmail.com>
2017-05-27 23:15:48 -0700

    updated UI

--- ServiceMe!/ViewController.swift
+++ ServiceMe!/ViewController.swift
@@ -8,14 +8,13 @@
 
 import UIKit
 import FirebaseAuth
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate  {
     
     @IBOutlet weak var error_label: UILabel!
     @IBOutlet weak var pass_text: UITextField!
     @IBOutlet weak var email_text: UITextField!
-    @IBOutlet weak var toggle_label: UISegmentedControl!
-    @IBAction func volunteerButton(_ sender: Any) {
-        if toggle_label.selectedSegmentIndex == 0 {
+    @IBAction func loginButton(_ sender: Any) {
+    
             Auth.auth().signIn(withEmail: email_text.text!, password: pass_text.text!) { (user, error) in
                 if user != nil {
                     //user exist
@@ -24,21 +23,28 @@ class ViewController: UIViewController {
                     self.error_label.isHidden = false
                     print("no dojo")
                 }
-            }
             
             
-        } else {
+    }
+    }
+    
+    @IBAction func signupButton(_ sender: Any) {
+        
             Auth.auth().createUser(withEmail: email_text.text!, password: pass_text.text!) { (user, error) in
                 // ...
+                
             }
-        }
-        
- 
+
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         error_label.isHidden = true
+        email_text.delegate = self
+        email_text.tag = 0 //Increment accordingly
+        pass_text.delegate = self
+        pass_text.tag = 0 //Increment accordingly
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -49,7 +55,51 @@ class ViewController: UIViewController {
     @IBAction func togle_click(_ sender: Any) {
         
     }
+    
+
+    //add just the bottom line to text fields to our view
+    override func viewDidLayoutSubviews() {
+        
+        let lineColor = UIColor.white
+        email_text.setBottomLine(borderColor: lineColor)
+        pass_text.setBottomLine(borderColor: lineColor)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        // Try to find next responder
+        if let nextField = textField.superview?.viewWithTag(textField.tag + 1) as? UITextField {
+            nextField.becomeFirstResponder()
+        } else {
+            // Not found, so remove keyboard.
+            textField.resignFirstResponder()
+        }
+        // Do not add a line break
+        return false
+    }
+    
+    
 
 
 }
+
+//this is to get the text field to just have bottom line
+extension UITextField {
+    
+    func setBottomLine(borderColor: UIColor) {
+        
+        self.borderStyle = UITextBorderStyle.none
+        self.backgroundColor = UIColor.clear
+        
+        let borderLine = UIView()
+        let height = 1.0
+        borderLine.frame = CGRect(x: 0, y: Double(self.frame.height) - height, width: Double(self.frame.width), height: height)
+        
+        borderLine.backgroundColor = borderColor
+        self.addSubview(borderLine)
+    }
+    
+}
+
 
